77fda6b8a4de103242f400f565cf31aab0e1911f
Marietta Kasprzak <marietta.kasprzak@gmail.com>
2023-02-02 22:19:54 +0100

    commit with handlebars

--- server.js
+++ server.js
@@ -1,38 +1,43 @@
 import express from 'express';
 import path from 'path';
+import { engine } from 'express-handlebars';
 
 const app = express();
 const __dirname = path.resolve();
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+app.set('views', './views');
 
 app.use(express.static(path.join(__dirname, '/public')));
 
-app.use((req, res, next) => {
-  res.show = (name) => {
-    res.sendFile(path.join(__dirname, `/views/${name}`));
-  };
-  next();
-});
-
-app.use('/user', (req, res) => {
-  res.show('forbidden.html');
-});
-
 app.get('/', (req, res) => {
-  res.show('home.html');
-});
-
-app.get('/home', (req, res) => {
-  res.show('home.html');
-});
-
-app.get('/about', (req, res) => {
-  res.show('about.html');
-});
-
-app.use((req, res) => {
-  res.status(404).show('404.html');
-});
-
-app.listen(8000, () => {
-  console.log('Server is running on port: 8000');
-});
+    res.render('layouts/main');
+  });
+  
+  app.get('/hello/:name', (req, res) => {
+    res.render('hello', { name: req.params.name });
+  });
+  
+  app.get('/about', (req, res) => {
+    res.render('about');
+  });
+  
+  app.get('/contact', (req, res) => {
+    res.render('contact');
+  });
+  
+  app.get('/info', (req, res) => {
+    res.render('info');
+  });
+  
+  app.get('/history', (req, res) => {
+    res.render('history');
+  });
+  
+  app.use((req, res) => {
+    res.status(404).send('404 not found...');
+  })
+  
+  app.listen(8000, () => {
+    console.log('Server is running on port: 8000');
+  });
